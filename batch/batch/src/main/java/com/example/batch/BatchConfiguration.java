cbef6d895dbfcdf7a58bea3d2231311ae6732cb7
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-07-15 10:53:39 +0200

    Adapt Spring Batch smoke tests to Spring Boot 4.0

--- batch/batch/src/main/java/com/example/batch/BatchConfiguration.java
+++ batch/batch/src/main/java/com/example/batch/BatchConfiguration.java
@@ -19,12 +19,12 @@ package com.example.batch;
 import javax.sql.DataSource;
 
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
